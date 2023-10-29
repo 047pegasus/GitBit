9e590ba8928fe14bb80d66450df8f13e3f592705
adityasachan24@gmail.com <adityasachan24@gmail.com>
2023-10-29 07:38:58 +0530

    added

--- gitbit/lib/screens/leaderboard.dart
+++ gitbit/lib/screens/leaderboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gitbit/welcome.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class Leaderboard extends StatefulWidget {
   const Leaderboard({super.key});
@@ -10,6 +13,29 @@ class Leaderboard extends StatefulWidget {
 class _LeaderboardState extends State<Leaderboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MyColors.darkGrey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Leaderboard',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          Lottie.asset(
+              'assets/0NLjEMKwaN.json'), // Replace with your Lottie animation file
+          SizedBox(height: 20),
+          Text(
+            'We put the laughter in leaderboard',
+            style:
+                GoogleFonts.openSans(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
